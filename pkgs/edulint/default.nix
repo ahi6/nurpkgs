2b42d7626346be6cdc760fc43fcccadd45831157
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-09-26 13:42:52 +0200

    Add packages

---
+++ pkgs/edulint/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  wheel,
+  pylint,
+  dataclasses-json,
+  tomli,
+  requests,
+  platformdirs,
+  loguru,
+  flake8,
+  flake8_json,
+  callPackage,
+  pkgs,
+}:
+buildPythonPackage rec {
+  pname = "edulint";
+  version = "4.2.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-HghX6OBG+r2rJQUspnJHWZicnjZCgDk8MxoWaxAABjA=";
+  };
+
+  # do not run tests
+  doCheck = false;
+
+  # specific to buildPythonPackage, see its reference
+  pyproject = true;
+  build-system = [
+    setuptools
+  ];
+  dependencies = [
+    flake8
+    # (callPackage ../flake8_json.nix {})
+    flake8_json
+    pylint
+    dataclasses-json
+    tomli
+    requests
+    platformdirs
+    loguru
+  ];
+
+  pythonRelaxDeps = true;
+
+  meta = {
+    description = "Python linter aimed at helping novice programmers improve their coding style";
+    homepage = "https://pypi.org/project/flake8-json";
+    license = lib.licenses.gpl3Only;
+  };
+}
