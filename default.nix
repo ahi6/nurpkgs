ec850abb8f7f14e59b49a1dd32822002392f6519
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-09-26 14:03:15 +0200

    I still need rec; also delete what's unneeded

--- default.nix
+++ default.nix
@@ -1,19 +1,6 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
 { pkgs ? import <nixpkgs> { } }:
 
-{
-  # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
-
+rec {
   flake8_json = pkgs.python3Packages.callPackage ./pkgs/flake8_json { };
   edulint = pkgs.python3Packages.callPackage ./pkgs/edulint { inherit flake8_json; };
 }
